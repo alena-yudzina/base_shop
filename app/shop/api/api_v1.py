e5c149f45eac7539dc313ed144bb49566722babe
elena <ep.yudzina@gmail.com>
2023-10-28 12:09:10 +0300

    add create_order method

--- app/shop/api/api_v1.py
+++ app/shop/api/api_v1.py
@@ -1,16 +1,35 @@
+from django.db import IntegrityError
 from ninja import Router
+from ninja.errors import HttpError
 from ninja.pagination import PageNumberPagination, paginate
 from shop import api_models, models
+from http import HTTPStatus
 
 router = Router()
 
 
 @router.get(
     "/products",
-    response=list[api_models.ProductSchema],
-    tags=["products"],
+    response=list[api_models.ProductOut],
     summary="Получить список товаров",
 )
 @paginate(PageNumberPagination, page_size=10)
 def get_products(request):
     return models.Product.objects.all()
+
+
+@router.post(
+    "/create_order",
+    summary="Создать заказ",
+    response=api_models.OrderOut
+)
+def create_order(request, order: api_models.OrderIn):
+    created_order = models.Order.objects.create(
+        price=sum(product.price for product in order.products)
+    )
+    try:
+        created_order.products.add(*(product.id for product in order.products))
+    except IntegrityError:
+        created_order.delete()
+        raise HttpError(HTTPStatus.BAD_REQUEST, 'Wrong products ids')
+    return api_models.OrderOut(id=created_order.id)
--- app/shop/api_models.py
+++ app/shop/api_models.py
@@ -1,8 +1,24 @@
-from ninja import ModelSchema
-from shop.models import Product
+from ninja import ModelSchema, Schema
+from shop.models import Order, Product
 
 
-class ProductSchema(ModelSchema):
+class ProductOut(ModelSchema):
     class Config:
         model = Product
         model_fields = "__all__"
+
+
+class ProductIn(ModelSchema):
+    class Config:
+        model = Product
+        model_fields = ["id", "price"]
+
+
+class OrderIn(Schema):
+    products: list[ProductIn]
+
+
+class OrderOut(ModelSchema):
+    class Config:
+        model = Order
+        model_fields = ["id", ]
