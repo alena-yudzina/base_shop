f5d0a8af3ecb2a4bb96cb7eb768192b64f1b4f4d
elena <ep.yudzina@gmail.com>
2023-10-28 12:28:04 +0300

    add create payment method

--- app/shop/api/api_v1.py
+++ app/shop/api/api_v1.py
@@ -33,3 +33,24 @@ def create_order(request, order: api_models.OrderIn):
         created_order.delete()
         raise HttpError(HTTPStatus.BAD_REQUEST, 'Wrong products ids')
     return api_models.OrderOut(id=created_order.id)
+
+
+@router.post(
+    "/create_payment",
+    summary="Создать платеж",
+    response=api_models.PaymentOut
+)
+def create_payment(request, payment: api_models.PaymentIn):
+
+    order = models.Order.objects.filter(id=payment.order_id).first()
+    if not order:
+        raise HttpError(HTTPStatus.BAD_REQUEST, 'Wrong order id')
+    try:
+        payment = models.Payment.objects.create(
+            amount=order.price,
+            payment_type=payment.payment_type,
+            order=order
+        )
+    except IntegrityError:
+        raise HttpError(HTTPStatus.BAD_REQUEST, 'Payment already exists')
+    return api_models.PaymentOut(id=payment.id)
--- app/shop/api_models.py
+++ app/shop/api_models.py
@@ -1,17 +1,18 @@
 from ninja import ModelSchema, Schema
-from shop.models import Order, Product
-
-
-class ProductOut(ModelSchema):
-    class Config:
-        model = Product
-        model_fields = "__all__"
+from shop.models import Order, Product, Payment
+from enum import Enum
 
 
 class ProductIn(ModelSchema):
     class Config:
         model = Product
         model_fields = ["id", "price"]
+
+
+class ProductOut(ModelSchema):
+    class Config:
+        model = Product
+        model_fields = "__all__"
 
 
 class OrderIn(Schema):
@@ -21,4 +22,26 @@ class OrderIn(Schema):
 class OrderOut(ModelSchema):
     class Config:
         model = Order
+        model_fields = ["id", ]
+
+
+class ProductIn(ModelSchema):
+    class Config:
+        model = Product
+        model_fields = ["id", "price"]
+
+
+class PaymentTypes(str, Enum):
+    CASH = "CASH"
+    CARD = "CARD"
+
+
+class PaymentIn(Schema):
+    order_id: int
+    payment_type: PaymentTypes
+
+
+class PaymentOut(ModelSchema):
+    class Config:
+        model = Payment
         model_fields = ["id", ]
