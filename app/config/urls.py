b30b5c231210df55b229740c6813ded9c28b7708
elena <ep.yudzina@gmail.com>
2023-10-29 10:37:51 +0300

    add url pattern

--- app/config/urls.py
+++ app/config/urls.py
@@ -1,4 +1,6 @@
 from config.api.api_v1 import api as api_v1
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
@@ -6,3 +8,4 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/", api_v1.urls),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
